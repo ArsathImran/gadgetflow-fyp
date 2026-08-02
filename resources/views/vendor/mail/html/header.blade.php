e8b9bc05623dff8e3d22c77941a370d83b7cc244
Arsath Imran <arsathimran981@gmail.com>
2026-08-02 20:39:56 +0800

    email notification done

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,8 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-<img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('images/logo.png'))) }}" class="logo" alt="GadgetFlow">
+<a href="{{ $url }}" style="display: inline-block; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif; font-size: 26px; font-weight: 700; letter-spacing: -0.02em; color: #F7F8FB; text-decoration: none;">
+GadgetFlow
 </a>
 </td>
 </tr>
